7334d578269ad9c8d8e2518dcd35e7845ca9bb19
Danil Nikiforovykh <danil.nikiforovykh@netcracker.com>
2024-01-08 23:21:36 +0300

    walls

--- collections.js
+++ collections.js
@@ -7,7 +7,7 @@ sprout.src = "img/items/sprout.png"
 
 const tree0 = new Image(); tree0.src = "img/plants/tree0.png"
 const tree1 = new Image(); tree1.src = "img/plants/tree1.png"
-const tree2 = new Image(); tree2.src = "img/plants/tree2.png"
+const tree2 = new Image(); tree2.src = "img/plants/tree22.png"
 
 const bush1 = new Image(); bush1.src = "img/plants/bush1.png"
 const bush2 = new Image(); bush2.src = "img/plants/bush2.png"
@@ -17,6 +17,28 @@ const apple = new Image(); apple.src = "img/items/apple.png"
 const berry = new Image(); berry.src = "img/items/berry.png"
 
 const bed = new Image(); bed.src = "img/construction/bed.png"
+
+const wall = new Image(); wall.src = "img/construction/wall1.png"
+const wallHorizontal = new Image(); wallHorizontal.src = "img/construction/wall8.png"
+const wallVertical = new Image(); wallVertical.src = "img/construction/wall12.png"
+const wallCross = new Image(); wallCross.src = "img/construction/wall6.png"
+const wallNorthWest = new Image(); wallNorthWest.src = "img/construction/wall11.png"
+const wallNorth = new Image(); wallNorth.src = "img/construction/wall9.png"
+const wallNorthEast = new Image(); wallNorthEast.src = "img/construction/wall10.png"
+const wallEast = new Image(); wallEast.src = "img/construction/wall5.png"
+const wallSouthEast = new Image(); wallSouthEast.src = "img/construction/wall4.png"
+const wallSouth = new Image(); wallSouth.src = "img/construction/wall3.png"
+const wallSouthWest = new Image(); wallSouthWest.src = "img/construction/wall2.png"
+const wallWest = new Image(); wallWest.src = "img/construction/wall7.png"
+
+const commandCut = new Image(); commandCut.src = "img/commands/cut.png"
+
+const surfaceDirt1 = new Image(); surfaceDirt1.src = "img/surface/dirt1.png"
+const surfaceDirt2 = new Image(); surfaceDirt2.src = "img/surface/dirt2.png"
+const surfaceDirt3 = new Image(); surfaceDirt3.src = "img/surface/dirt3.png"
+
+const surfaceGrass1 = new Image(); surfaceGrass1.src = "img/surface/grass1.png"
+const surfaceGrass2 = new Image(); surfaceGrass2.src = "img/surface/grass3.png"
 
 let collections = {
 
@@ -45,8 +67,35 @@ let collections = {
                     type: "wood",
                     qty: 20
                 }
+            ],
+            inventory: [
+                {
+                    type: "wood",
+                    qty: 10
+                }
             ]
         },
+
+        wall: {
+            name: 'wall',
+            oriented: true,
+            orientedImg: {
+                default: wall,
+                horizontal: wallHorizontal,
+                vertical: wallVertical,
+                cross: wallCross,
+                north: wallNorth,
+                northEast: wallNorthEast,
+                east: wallEast,
+                southEast: wallSouthEast,
+                south: wallSouth,
+                southWest: wallSouthWest,
+                west: wallWest,
+                northWest: wallNorthWest
+            },
+            parts: [{type: 'wood', qty: 5}]
+        },
+
         tree: {
             name: "tree",
             growRate: 1,
