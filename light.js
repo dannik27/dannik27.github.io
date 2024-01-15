2438204d4e4275dad5bb55fff9f421e17a95660a
Danil Nikiforovykh <danil.nikiforovykh@netcracker.com>
2024-01-15 23:13:00 +0300

    light demo completed

--- light.js
+++ light.js
@@ -1,38 +1,38 @@
 
 
-const canvas = document.querySelector("#scene");
-const ctx = canvas.getContext("2d");
+// const canvas = document.querySelector("#scene");
+// const ctx = canvas.getContext("2d");
 
 let TILE_SIZE = 50
 
-let objects = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
-    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-]
+// let objects = [ 
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+//     [0, 0, 0, 1, 1, 1, 1, 0, 0, 0,],
+//     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
+//     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
+//     [0, 0, 0, 0, 0, 0, 1, 0, 0, 0,],
+//     [0, 0, 0, 1, 1, 1, 1, 0, 0, 0,],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+// ]
 
-lights = [
-    {
-        x: 2,
-        y: 4,
-        radius: 3
-    },
-    {
-        x: 4,
-        y: 7,
-        radius: 2
-    }
-]
+// lights = [
+//     {
+//         x: 2,
+//         y: 4,
+//         radius: 3
+//     },
+//     {
+//         x: 4,
+//         y: 7,
+//         radius: 2
+//     }
+// ]
 
-ctx.fillStyle = 'white';
-ctx.fillRect(0,0,500,500);
+// ctx.fillStyle = 'white';
+// ctx.fillRect(0,0,500,500);
 
 // for (let y = 0; y < objects.length; y++) {
 //     for (let x = 0; x < objects[0].length; x++) {
@@ -90,40 +90,52 @@ ctx.fillRect(0,0,500,500);
 // ctx.fill()
 
 
-
+function distanceBetween(a, b) {
+    return Math.sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y))
+}
 
 function generateRays(objects, center, radius) {
     let rays = []
 
     for (obj of objects) {
-        rays.push({x: obj.x, y: obj.y})
-        rays.push({x: obj.x, y: obj.y + 50})
-        rays.push({x: obj.x + 50, y: obj.y})
-        rays.push({x: obj.x + 50, y: obj.y + 50})
+        if (distanceBetween(center, {x: obj.x, y: obj.y}) < radius)
+            rays.push({x: obj.x, y: obj.y})
+        if (distanceBetween(center, {x: obj.x, y: obj.y + obj.h}) < radius)
+            rays.push({x: obj.x, y: obj.y + obj.h})
+        if (distanceBetween(center, {x: obj.x + obj.w, y: obj.y}) < radius)
+            rays.push({x: obj.x + obj.w, y: obj.y})
+        if (distanceBetween(center, {x: obj.x + obj.w, y: obj.y + obj.h}) < radius)
+            rays.push({x: obj.x + obj.w, y: obj.y + obj.h})
     
         let x1 = obj.x - center.x
-        let x2 = obj.x + 50 - center.x
+        let x2 = obj.x + obj.w - center.x
         let y1 = center.y - obj.y
-        let y2 = center.y - (obj.y + 50)
+        let y2 = center.y - (obj.y + obj.h)
         let circleYinX1 = Math.floor(Math.sqrt(radius * radius - x1 * x1) * 100) / 100
         let circleYinX2 = Math.floor(Math.sqrt(radius * radius - x2 * x2) * 100) / 100
         let circleXinY1 = Math.floor(Math.sqrt(radius * radius - y1 * y1) * 100) / 100
         let circleXinY2 = Math.floor(Math.sqrt(radius * radius - y2 * y2) * 100) / 100
-        if (x1 < 0) circleXinY1 = -circleXinY1
-        if (x1 < 0) circleXinY2 = -circleXinY2
+        // if (x1 < 0) circleXinY1 = -circleXinY1
+        // if (x1 < 0) circleXinY2 = -circleXinY2
         if (y1 < 0) circleYinX1 = -circleYinX1
         if (y1 < 0) circleYinX2 = -circleYinX2
-        if (circleYinX1 > y2 && circleYinX1 < y1) {
+        if (circleYinX1 != 0 && circleYinX1 > y2 && circleYinX1 < y1) {
             rays.push({x: obj.x, y: center.y - circleYinX1, arc: true})
         }
-        if (circleYinX2 > y2 && circleYinX2 < y1) {
-            rays.push({x: obj.x + 50, y: center.y - circleYinX2, arc: true})
+        if (circleYinX2 != 0 && circleYinX2 > y2 && circleYinX2 < y1) {
+            rays.push({x: obj.x + obj.w, y: center.y - circleYinX2, arc: true})
         }
-        if (circleXinY1 > x1 && circleXinY1 < x2) {
+        if (circleXinY1 != 0 && circleXinY1 > x1 && circleXinY1 < x2) {
             rays.push({x: center.x + circleXinY1, y: obj.y, arc: true})
         }
-        if (circleXinY2 > x1 && circleXinY2 < x2) {
-            rays.push({x: center.x + circleXinY2, y: obj.y + 50, arc: true})
+        if (circleXinY1 != 0 && -circleXinY1 > x1 && -circleXinY1 < x2) {
+            rays.push({x: center.x + -circleXinY1, y: obj.y, arc: true})
+        }
+        if (circleXinY2 != 0 && circleXinY2 > x1 && circleXinY2 < x2) {
+            rays.push({x: center.x + circleXinY2, y: obj.y + obj.h, arc: true})
+        }
+        if (circleXinY2 != 0 && -circleXinY2 > x1 && -circleXinY2 < x2) {
+            rays.push({x: center.x + -circleXinY2, y: obj.y + obj.h, arc: true})
         }
     }
     return rays
@@ -136,16 +148,16 @@ function calculateJoinPoints(ray, objects, center, radius) {
     for(obj of objects) {
 
         // check if point is in the same quarter
-        if(((obj.x + (TILE_SIZE / 2) - center.x) * (ray.x - center.x) < 0) || ((obj.y + (TILE_SIZE / 2) - center.y) * (ray.y - center.y) < 0)) {
+        if(((obj.x + (obj.w / 2) - center.x) * (ray.x - center.x) < 0) || ((obj.y + (obj.h / 2) - center.y) * (ray.y - center.y) < 0)) {
             continue
         }
 
         let objCandidates = [] 
 
         let x1 = obj.x - center.x
-        let x2 = obj.x + 50 - center.x
+        let x2 = obj.x + obj.w - center.x
         let y1 = center.y - obj.y
-        let y2 = center.y - (obj.y + 50)
+        let y2 = center.y - (obj.y + obj.h)
         let rayYinX1 = Math.floor(x1 * ray.alpha * 100) / 100
         let rayYinX2 = Math.floor(x2 * ray.alpha * 100) / 100
         let rayXinY1 = Math.floor(y1 / ray.alpha * 100) / 100
@@ -154,13 +166,13 @@ function calculateJoinPoints(ray, objects, center, radius) {
             objCandidates.push({x: obj.x, y: center.y - rayYinX1})
         }
         if (rayYinX2 >= y2 && rayYinX2 <= y1) {
-            objCandidates.push({x: obj.x + 50, y: center.y - rayYinX2})
+            objCandidates.push({x: obj.x + obj.w, y: center.y - rayYinX2})
         }
         if (rayXinY1 > x1 && rayXinY1 < x2) {
             objCandidates.push({x: center.x + rayXinY1, y: obj.y})
         }
         if (rayXinY2 > x1 && rayXinY2 < x2) {
-            objCandidates.push({x: center.x + rayXinY2, y: obj.y + 50})
+            objCandidates.push({x: center.x + rayXinY2, y: obj.y + obj.h})
         }
 
         if (objCandidates.length == 1) {
@@ -172,25 +184,13 @@ function calculateJoinPoints(ray, objects, center, radius) {
          
     }
 
-    // candidates.sort((a, b) => {
-    //     let x1 = center.x - a.x
-    //     let y1 = center.y - a.y
-    //     let hyppo1 = Math.sqrt(x1 * x1 + y1 * y1)
-
-    //     let x2 = center.x - b.x
-    //     let y2 = center.y - b.y
-    //     let hyppo2 = Math.sqrt(x2 * x2 + y2 * y2)
-
-    //     return hyppo1 - hyppo2
-    // })
-
     candidates = candidates.map(a => {
         let x = center.x - a.x
         let y = center.y - a.y
         let hyppo = Math.sqrt(x * x + y * y)
         return {...a, hyppo: hyppo}
     })
-        // .filter(a => a.hyppo <= radius)
+        .filter(a => Math.floor(a.hyppo) <= radius)
         .sort((a, b) => {
             return a.hyppo - b.hyppo
         })
@@ -201,25 +201,26 @@ function calculateJoinPoints(ray, objects, center, radius) {
         return ray
     }
 
-    if (candidates.length == 1) {
+    if (candidates.length == 1 && candidates[0].corner) {
         let candidate = candidates[0]
         candidate.corner = true
 
-        let x = candidate.x - c.x
-        let y = candidate.y - c.y
+        let x = candidate.x - center.x
+        let y = candidate.y - center.y
         let r = Math.sqrt(x*x + y*y)
 
         let X = radius * x / r
         let Y = radius * y / r
 
         candidate.second = {
-            x: c.x + X,
-            y: c.y + Y,
+            x: center.x + X,
+            y: center.y + Y,
             arc: true
         }
         
     } else if (candidates[0].corner) {
-        let nonCorner = candidates.find(cand => !cand.corner)
+        // let nonCorner = candidates.find(cand => !cand.corner)
+        let nonCorner = candidates[1]
         if (nonCorner) {
             candidates[0].second = {
                 x: nonCorner.x,
@@ -229,7 +230,7 @@ function calculateJoinPoints(ray, objects, center, radius) {
     }
 
     
-    if(candidates[0].x != ray.x || candidates[0].y != ray.y) {
+    if(Math.floor(candidates[0].x) != Math.floor(ray.x) || Math.floor(candidates[0].y) != Math.floor(ray.y)) {
         ray.arc = false
     }
     ray.x = candidates[0].x
@@ -273,7 +274,7 @@ function isBlindRay(ray, center, radius) {
     let y = center.y - ray.y
     let hyppo = Math.sqrt(x * x + y * y)
 
-    return Math.floor(hyppo * 1000) / 1000 > radius
+    return Math.floor(hyppo) > Math.floor(radius)
 }
 
 function drawRay(ray, center, radius) {
@@ -312,21 +313,37 @@ function drawRay(ray, center, radius) {
     return ray
 }
 
-function buildPath(rays, radius) {
+function dropDuplicates(rays) {
+
+    for (let i = 1; i < rays.length; i++) {
+        if (rays[i].x == rays[i - 1].x && rays[i].y == rays[i - 1].y) {
+            rays.splice(i, 1)
+            i--
+        }
+    }
+
+}
+
+function buildPath(rays, center, radius) {
     let path = new Path2D()
 
+    if (rays.length == 0) {
+        path.arc(center.x, center.y, radius, 0, 2 * Math.PI, false)
+        return path
+    }
+
     let first = rays[0]
-    let raysCopy = rays.slice(1)
+    let raysCopy = rays.slice()
     raysCopy.push(first)
 
     path.moveTo(rays[0].x, rays[0].y)
     let arcStart = null
-    for (let i = 0; i < rays.length; i++) {
+    for (let i = 1; i < rays.length + 1; i++) {
         let ray = raysCopy[i]
         console.log(ray.x + " " + ray.y + " " + ray.angle)
         
         if (arcStart) {
-            path.arc(c.x, c.y, radius, -arcStart.angle, -ray.angle, true)
+            path.arc(center.x, center.y, radius, -arcStart.angle, -ray.angle, true)
             if (ray.second) {
                 path.lineTo(ray.x, ray.y)
             }
@@ -334,8 +351,17 @@ function buildPath(rays, radius) {
         } else {
 
             if (ray.second && !ray.second.arc) {
-                path.lineTo(ray.second.x, ray.second.y)
-                path.lineTo(ray.x, ray.y)
+                let previous = raysCopy[i - 1]
+                let distanceToFirst = (ray.x - previous.x) * (ray.x - previous.x) + (ray.y - previous.y) * (ray.y - previous.y)
+                let distanceToSecond = (ray.second.x - previous.x) * (ray.second.x - previous.x) + (ray.second.y - previous.y) * (ray.second.y - previous.y)
+                if (distanceToFirst < distanceToSecond) {
+                    path.lineTo(ray.x, ray.y)
+                    path.lineTo(ray.second.x, ray.second.y)
+                } else {
+                    path.lineTo(ray.second.x, ray.second.y)
+                    path.lineTo(ray.x, ray.y)
+                }
+                
             } else if (ray.second && ray.second.arc) {
                 path.lineTo(ray.x, ray.y)
                 path.lineTo(ray.second.x, ray.second.y)
@@ -373,9 +399,9 @@ function drawObjects(objects) {
     for(let i = 0; i < objects.length; i++) {
         let obj = objects[i]
         ctx.fillStyle = 'green'
-        ctx.fillRect(obj.x, obj.y, 50, 50)
+        ctx.fillRect(obj.x, obj.y, obj.w, obj.h)
         ctx.fillStyle = 'black'
-        ctx.fillText(obj.x + " " + obj.y, obj.x+(TILE_SIZE/2),obj.y+(TILE_SIZE/2));
+        ctx.fillText(obj.x + " " + obj.y, obj.x+(obj.w/2),obj.y+(obj.h/2));
     }
 }
 
@@ -388,6 +414,7 @@ function filterSecondaryPoints(ray, center, radius) {
 
 function createLightMap(objects, lights) {
 
+    let path = new Path2D()
     for (light of lights) {
         let center = {
             x: light.x,
@@ -403,45 +430,18 @@ function createLightMap(objects, lights) {
             .sort((a, b) => {
                 return a.angle - b.angle
             })
-        .map(ray => drawRay(ray, c, radius))
 
-        drawObjects(points)
+        dropDuplicates(rays)
+        // rays.map(ray => drawRay(ray, center, radius))
 
-        let path = buildPath(rays, radius)
-        ctx.fillStyle = 'rgb(255, 255, 0, 0.4)'
-        ctx.fill(path)
-        ctx.stroke(path)
-
-
+        path.addPath(buildPath(rays, center, radius))
     }
 
+    return path
+
 }
 
 
-let c = {
-    x: 325,
-    y: 325
-}
-
-// let R = 250
-
-let points = [
-    
-    { x: 400, y: 150 },
-    { x: 350, y: 150 },
-    {x: 400,y: 200},
-    {x: 500,y: 200},
-    {x: 300,y: 150},
-    { x: 150, y: 100},
-    { x: 150,y: 300},
-    { x: 150,y: 400},
-    {x: 450,y: 400},
-]
-
-// createLightMap(points, [{x: c.x, y: c.y, radius: R}])
-createLightMap(points, [{x: 325, y: 325, radius: 150}])
-
-// let rays = generateRays(points, c, R)
 
 
 
---
+++ light_demo.js
@@ -0,0 +1,120 @@
+
+const canvas = document.querySelector("#scene");
+const ctx = canvas.getContext("2d");
+
+
+let blockSize = 40
+
+let points = [
+    
+    { x: 400, y: 160, w: blockSize, h: blockSize },
+    { x: 360, y: 160, w: blockSize, h: blockSize },
+    {x: 400,y: 200, w: blockSize, h: blockSize},
+    {x: 520,y: 200, w: blockSize, h: blockSize},
+    {x: 320,y: 160, w: blockSize, h: blockSize},
+    { x: 160, y: 120, w: blockSize, h: blockSize},
+    { x: 160,y: 320, w: blockSize, h: blockSize},
+    { x: 160,y: 400, w: blockSize, h: blockSize},
+    { x: 160,y: 440, w: blockSize, h: blockSize},
+    { x: 160,y: 480, w: blockSize, h: blockSize},
+    { x: 160,y: 520, w: blockSize, h: blockSize},
+    { x: 200,y: 520, w: blockSize, h: blockSize},
+    { x: 240,y: 520, w: blockSize, h: blockSize},
+    { x: 280,y: 520, w: blockSize, h: blockSize},
+    { x: 320,y: 520, w: blockSize, h: blockSize},
+    { x: 360,y: 520, w: blockSize, h: blockSize},
+    { x: 360,y: 480, w: blockSize, h: blockSize},
+    { x: 360,y: 440, w: blockSize, h: blockSize},
+    { x: 360,y: 400 , w: blockSize, h: blockSize},
+    { x: 320,y: 400 , w: blockSize, h: blockSize},
+    { x: 280,y: 400 , w: blockSize, h: blockSize},
+    { x: 200,y: 400 , w: blockSize, h: blockSize},
+    {x: 460,y: 400, w: blockSize, h: blockSize},
+]
+
+let lights = [
+    {x: 480, y: 280, radius: 120}, 
+    {x: 340, y: 460, radius: 60}, 
+    {x: 260, y: 500, radius: 60}, 
+    {x: 220, y: 340, radius: 80},
+    // {x: 280, y: 280, radius: 120},
+    // {x: 330, y: 130, radius: 120}
+]
+
+// createLightMap(points, [{x: c.x, y: c.y, radius: R}])
+let path = createLightMap(points, lights)
+// createLightMap(points, [{x: 275, y: 275, radius: 120}])
+// createLightMap(points, [{x: 325, y: 125, radius: 120}])
+// ctx.clip()
+
+
+
+ctx.fillStyle = 'white'
+ctx.fillRect(0, 0, 600, 600)
+drawObjects(points)
+
+ctx.fillStyle = 'red'
+ctx.fillRect(250, 150, 20, 20)
+ctx.fillRect(250, 300, 20, 20)
+// ctx.fillStyle = 'rgb(255, 255, 0, 0.4)'
+// ctx.fill(path)
+// ctx.stroke(path)
+// ctx.clip(path)
+
+// ctx.fillStyle = 'rgb(0, 0, 0, 0.3)'
+// ctx.fillRect(0, 0, 600, 600)
+
+
+
+var can2 = document.createElement('canvas');
+can2.width = canvas.width;
+can2.height = canvas.height;
+var ctx2 = can2.getContext('2d');
+ctx2.clip(path)
+ctx2.drawImage(canvas, 0, 0)
+
+for (light of lights) {
+    
+    ctx2.fillStyle = 'rgba(255, 255, 0, 0.06)'
+    ctx2.beginPath()
+    ctx2.arc(light.x, light.y, light.radius, 0, 2 * Math.PI, false)
+    ctx2.fill() 
+    ctx2.beginPath()
+    ctx2.arc(light.x, light.y, light.radius - 20, 0, 2 * Math.PI, false)
+    ctx2.fill() 
+    ctx2.beginPath()
+    ctx2.arc(light.x, light.y, light.radius - 40, 0, 2 * Math.PI, false)
+    ctx2.fill() 
+    ctx2.beginPath()
+    ctx2.arc(light.x, light.y, light.radius - 60, 0, 2 * Math.PI, false)
+    ctx2.fill() 
+    
+}
+
+ctx.fillStyle = 'rgb(0, 0, 0, 0.8)'
+ctx.fillRect(0, 0, 600, 600)
+ctx.drawImage(can2, 0, 0);
+
+
+
+
+
+
+// ctx2.drawImage(canvas, 0, 0)
+// ctx.fillStyle = 'rgb(0, 0, 0, 0.5)';
+// ctx.fillRect(0,0,500,500); // fill the entire first canvas
+// ctx.drawImage(can2, 0, 0);
+
+
+// ctx.beginPath();
+// for (light of lights) {
+    
+//     ctx.fillStyle = 'rgba(255,255,0,0.05)'
+//     ctx.arc(light.x * TILE_SIZE + (TILE_SIZE / 2), light.y * TILE_SIZE + (TILE_SIZE / 2), light.radius * TILE_SIZE, 0, 2 * Math.PI, false)
+//     ctx.lineWidth = 3
+//     ctx.setLineDash([])
+//     // ctx.stroke()
+//     ctx.closePath()
+    
+// }
+// ctx.fill()
